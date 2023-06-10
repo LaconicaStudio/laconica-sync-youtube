c46bec53121795ccc6e0d519c63f970f123df1b4
Anna Holubiatnikova <holubiatnikova.anna@gmail.com>
2023-06-10 10:48:49 +0200

    Config changed

--- src/config/laconica.sync.php
+++ src/config/laconica.sync.php
@@ -4,13 +4,11 @@ return [
         'youtube' => [
             'redirect_endpoint' => 'https://accounts.google.com/o/oauth2/v2/auth',
             'request_token_endpoint' => 'https://oauth2.googleapis.com/token',
-            'redirect_params' => 'include_granted_scopes=true&access_type=offline&prompt=consent&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.channel-memberships.creator+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.force-ssl+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutubepartner+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutubepartner-channel-audit&auth_type=rerequest&config_id=546398514232015',
-            'request_token_params' => 'access_type=offline&grant_type=authorization_code&prompt=consent',
-            'refresh_token_params' => 'access_type=offline&grant_type=refresh_token&prompt=consent',
-            // 'client_id' => env('YOUTUBE_CLIENT_ID', '1093101282496-jhqbs0njj03lj1117vpee80h6m7v73sc.apps.googleusercontent.com'),
-            // 'client_secret' => env('YOUTUBE_CLIENT_SECRET', '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'),
-            'client_id' => env('YOUTUBE_CLIENT_ID', '478844683299-kl3efvdk3irdurjvs3vhikbrtgn36gc1.apps.googleusercontent.com'),
-            'client_secret' => env('YOUTUBE_CLIENT_SECRET', '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'),
+            'redirect_params' => 'include_granted_scopes=true&access_type=offline&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.channel-memberships.creator+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.force-ssl+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutubepartner+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutubepartner-channel-audit&auth_type=rerequest&config_id=546398514232015',
+            'request_token_params' => 'access_type=offline&grant_type=authorization_code',
+            'refresh_token_params' => 'access_type=offline&grant_type=refresh_token',
+            'client_id' => env('YOUTUBE_CLIENT_ID', '1093101282496-jhqbs0njj03lj1117vpee80h6m7v73sc.apps.googleusercontent.com'),
+            'client_secret' => env('YOUTUBE_CLIENT_SECRET', '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'),
             'api_prefix' => 'https://youtube.googleapis.com/',
             'post_configuration_required' => false,            
             'endpoints' => [
